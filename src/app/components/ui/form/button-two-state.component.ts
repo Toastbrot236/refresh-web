18e7e853325f59e4f8d96f13d6350e8f0decf26b
ToasterTheBrot <95178340+Toastbrot236@users.noreply.github.com>
2025-02-15 14:06:42 +0100

    Heart, Queue and Play Now options for levels (#1)
    * Implement hearting and queueing
    
    * improve hearting and queueing levels UI, implement Play Now
    
    * 3 buttons in a row while also fixing a bug with ButtonOrNavItem
    
    * Implement LevelRelation, fix level header buttons bugs and revert to 2 buttons max, ability to add username query parameter to level categories
    
    * very minor efficiency improvement, refactors and other misc changes for level button area
    
    * Change and remove some things i forgot about
    
    * Rename a component and refactor some code into its own component for reusability
    
    * Remove debug Play Now button
    
    * Formatting and other minor things i forgot
    
    * This change isnt nessesary yet, undoing
    
    * Remove and replace some no longer nessesary CSS classes and HTML elements, titleSubtext is next to title again
    
    * Revert some other not yet/no longer nessesary changes
    
    * try and see if this works

---
+++ src/app/components/ui/form/button-two-state.component.ts
@@ -0,0 +1,34 @@
+import {Component, Input} from '@angular/core';
+import {ButtonComponent} from "./button.component";
+import {IconProp} from "@fortawesome/fontawesome-svg-core";
+
+@Component({
+    selector: 'app-button-two-state',
+    standalone: true,
+    imports: [
+        ButtonComponent
+    ],
+    template: `
+        @if (state) {
+            <app-button [text]="textAlternative" [icon]="iconAlternative" [color]="colorAlternative" [enabled]="enabled"></app-button>
+        }
+        @else {
+            <app-button [text]="text" [icon]="icon" [color]="color" [enabled]="enabled"></app-button>
+        }
+    `,
+    styles: ``
+})
+export class ButtonTwoStateComponent {
+    @Input({required: true}) state: boolean = false;
+
+    @Input({required: true}) text: string = "Activate";
+    @Input({required: true}) textAlternative: string = "Deactivate";
+
+    @Input() icon: IconProp | undefined;
+    @Input() iconAlternative: IconProp | undefined;
+
+    @Input() color: string = "bg-primary";
+    @Input() colorAlternative: string = "bg-secondary";
+
+    @Input() enabled: boolean = true;
+}
