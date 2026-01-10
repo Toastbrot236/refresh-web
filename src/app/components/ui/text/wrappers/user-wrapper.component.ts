954c94584d386f94ab7da62f24ef8a330f7aafcd
Toaster2 <brottoastlemon@gmail.com>
2026-01-10 18:02:07 +0100

    Allow positioning components next to UserWrapperComponent username aswell

--- src/app/components/ui/text/wrappers/user-wrapper.component.ts
+++ src/app/components/ui/text/wrappers/user-wrapper.component.ts
@@ -10,13 +10,20 @@ import { MinimalUser } from '../../../../api/types/users/minimal-user';
         UserRouterLinkComponent
     ],
     template: `
-    <div class="flex gap-x-1.5">
+    <div class="flex gap-x-2.5">
       <app-user-router-link [user]=user>
         <app-user-avatar class="ml-1" [user]=user [size]=48></app-user-avatar>
       </app-user-router-link>
       
-      <div class="flex flex-col">
-        <app-user-router-link [user]="user" class="font-bold">{{user.username}}</app-user-router-link>
+      <div class="flex flex-col grow">
+        <div class="flex flex-row">
+          <app-user-router-link [user]="user" class="font-bold text-[17px]">
+            <div class="flex flex-row">
+              {{user.username}}
+            </div>
+          </app-user-router-link>
+          <ng-content select="[next]"></ng-content>
+        </div>
         <ng-content></ng-content>
       </div>
     </div>
