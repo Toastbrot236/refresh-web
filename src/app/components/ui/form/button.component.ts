00ca344a21ac0f67d905dff6be39c338c1d5bfa4
Toaster2 <brottoastlemon@gmail.com>
2026-01-10 20:30:02 +0100

    Ability to customize padding for regular buttons

--- src/app/components/ui/form/button.component.ts
+++ src/app/components/ui/form/button.component.ts
@@ -10,8 +10,8 @@ import { NgClass } from "@angular/common";
         NgClass
     ],
     template: `
-    <button class="rounded px-4 py-1.5 hover:brightness-110 active:brightness-95 transition-[filter] disabled:grayscale" 
-      [ngClass]="color" [type]=type [disabled]="!enabled">
+    <button class="rounded hover:brightness-110 active:brightness-95 transition-[filter] disabled:grayscale" 
+      [ngClass]="color + ' px-' + xPadding + ' py-' + yPadding" [type]=type [disabled]="!enabled">
       @if (icon) {
         <fa-icon [icon]="icon" [ngClass]="text && text.length > 0 ? 'mr-1' : ''"></fa-icon>
       }
@@ -24,6 +24,8 @@ export class ButtonComponent {
   @Input({required: true}) text: string = "Button";
   @Input() icon: IconProp | undefined;
   @Input() color: string = "bg-secondary";
+  @Input() xPadding: string = "4";
+  @Input() yPadding: string = "1.5";
 
   @Input() type: "submit" | "reset" | "button" = "button";
 
